04833bfb4b88d636fbbfcfd7f65354d9308561be
Sean Feldman <feldman.sean@gmail.com>
2016-10-15 02:15:46 -0600

    connection string validation

--- src/Shared/RabbitMqConnectionString.cs
+++ src/Shared/RabbitMqConnectionString.cs
@@ -1,7 +1,17 @@
 ﻿namespace Shared
 {
+    using System;
+
     public class RabbitMqConnectionString
     {
         public static string Value => "ADD_YOUR_RABBITMQ_CONNECTION_STRING_HERE";
+
+        public static void EnsureConnectionStringIsProvided()
+        {
+            if (Value == "ADD_YOUR_RABBITMQ_CONNECTION_STRING_HERE")
+            {
+                throw new InvalidOperationException("Provide your connection string for RabbitMQ.");
+            }
+        }
     }
 }
